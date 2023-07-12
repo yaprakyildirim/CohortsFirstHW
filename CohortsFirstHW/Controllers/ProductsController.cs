e18a6591e9ddbc6752ff7ebe76b680784e278e7e
yaprakyildirimc <yyildirim@civil.com.tr>
2023-07-12 11:42:45 +0300

    Listing added

--- CohortsFirstHW/Controllers/ProductsController.cs
+++ CohortsFirstHW/Controllers/ProductsController.cs
@@ -1,5 +1,6 @@
 ﻿using CohortsFirstHW.Models;
 using Microsoft.AspNetCore.Mvc;
+using System;
 
 [ApiController]
 [Route("api/[controller]")]
@@ -7,17 +8,21 @@ public class ProductsController : ControllerBase
 {
     private static List<Product> products = new List<Product>
     {
-        new Product { Id = 1, Name = "Product 1", Description = "Description 1", Price = 9.99M },
-        new Product { Id = 2, Name = "Product 2", Description = "Description 2", Price = 19.99M },
-        // Daha fazla ürün ekleyebilirsiniz...
+        //ürünler listeleniyor
+        new Product { Id = 1, Name = "Shirt", Description = "Description 1", Price = 25.99M },
+        new Product { Id = 2, Name = "Skirt", Description = "Description 2", Price = 19.99M },
+        new Product { Id = 3, Name = "Jean", Description = "Description 2", Price = 59.99M },
+        new Product { Id = 4, Name = "T-shirt", Description = "Description 2", Price = 20.00M },
     };
 
+    //ürünlerin tamamını çağırıyoruz
     [HttpGet]
     public IActionResult Get()
     {
         return Ok(products);
     }
 
+    //ürünleri id göre getiriyoruz
     [HttpGet("{id}")]
     public IActionResult Get(int id)
     {
@@ -29,6 +34,8 @@ public class ProductsController : ControllerBase
         return Ok(product);
     }
 
+    //yeni bir ürün oluşturmayı sağlar.
+    //HTTP Post isteğiyle bu metoda bir Product nesnesi gönderilir ve bu nesne doğrudan model binding kullanılarak product parametresine aktarılır.
     [HttpPost]
     public IActionResult Post(Product product)
     {
@@ -38,9 +45,11 @@ public class ProductsController : ControllerBase
         }
 
         products.Add(product);
+        //başarılı bir şekilde yeni bir kaynak oluşturulduğunu gösteren HTTP 201 status kodu ile birlikte döner. 
         return CreatedAtAction(nameof(Get), new { id = product.Id }, product);
     }
 
+    //belirli bir ürünü güncellemeyi sağlar. id ye göre
     [HttpPut("{id}")]
     public IActionResult Put(int id, Product product)
     {
@@ -59,9 +68,11 @@ public class ProductsController : ControllerBase
         existingProduct.Description = product.Description;
         existingProduct.Price = product.Price;
 
+        //başarılı bir şekilde bir ürün güncellendiğinde, NoContent sonucu döner.  HTTP 204 status döner
         return NoContent();
     }
 
+    //belirli ürünü silmeyi sağlar, id ye göre
     [HttpDelete("{id}")]
     public IActionResult Delete(int id)
     {
@@ -75,6 +86,7 @@ public class ProductsController : ControllerBase
         return NoContent();
     }
 
+    //listeleme ve sıralama işlevleri
     [HttpGet("list")]
     public IActionResult List(string name, string sort)
     {
@@ -85,6 +97,7 @@ public class ProductsController : ControllerBase
             result = result.Where(p => p.Name.Contains(name));
         }
 
+        // "desc" için azalan, "asc" için artan sıralama yapılır.
         if (!string.IsNullOrEmpty(sort) && sort == "desc")
         {
             result = result.OrderByDescending(p => p.Name);
--- CohortsFirstHW/Models/Product.cs
+++ CohortsFirstHW/Models/Product.cs
@@ -4,11 +4,16 @@ namespace CohortsFirstHW.Models
 {
     public class Product
     {
-        [Required]
+        //Product propertylerine bazı kısıtlar getirilmiştir
+        [Required]//girilmesi zorunlu alan
         public int Id { get; set; }
-        [Required(ErrorMessage = "Name is required")]
+
+        [Required(ErrorMessage = "Name is required")]//girilmesi zorunlu alan ve hata mesajı
         public string Name { get; set; }
+
+        [StringLength(maximumLength: 50, MinimumLength = 10)] //description için en az 10 en fazla 50 karakter uzunluğu kısıtı
         public string Description { get; set; }
+
         [Required]
         public decimal Price { get; set; }
     }
